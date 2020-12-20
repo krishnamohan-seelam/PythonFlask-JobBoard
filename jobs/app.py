eedfccf99ee626fdbd50559d6b254d9ef68b0715
krishnamohan-seelam <krishnamohan.seelam@outlook.com>
2020-12-20 13:51:17 +0530

    completed module4

--- jobs/app.py
+++ jobs/app.py
@@ -12,13 +12,14 @@ def open_connection():
     connection.row_factory  = sqlite3.Row
     return connection 
 
-def execute_sql(sql,values,commit=False,single=False):
+def execute_sql(sql,values=(),commit=False,single=False):
     connection = open_connection()
     cursor = connection.execute(sql,values,commit,single)
     if commit:
         results  = connection.commit()
     else:
         results  = cursor.fetchone() if single else cursor.fetchall()
+    cursor.close()
     return results
 
 @app.teardown_appcontext
@@ -26,9 +27,6 @@ def close_connection(exception):
     connection = getattr(g, '_connection', None)
     if connection is not None:
         connection.close()   
-
-
-
 
 @app.route('/')
 @app.route('/jobs')
